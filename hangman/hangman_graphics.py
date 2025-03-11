065e0270707d51efb3e1884288f8b57120bd943b
mamiksik <mamiksik@gmail.com>
2025-03-11 14:16:31 +0100

    Add hangman graphics

---
+++ hangman/hangman_graphics.py
@@ -0,0 +1,90 @@
+import time
+import turtle
+
+# Adjust the speed of the turtle 'fastest', 'fast', 'normal', 'slow', 'slowest'
+turtle.speed('slowest')
+
+# Available turtle shapes: 'arrow', 'turtle', 'circle', 'square', 'triangle', 'classic'
+turtle.shape('turtle')
+
+DEFAULT_PEN_COLOR = "black"
+
+def draw_circle(x, y, radius, pen_color):
+    turtle.penup()
+    turtle.pencolor(pen_color)
+    turtle.goto(x, y - radius)
+    turtle.pendown()
+    turtle.circle(radius)
+
+
+def draw_line(start_x, start_y, end_x, end_y, pen_color):
+    turtle.penup()
+    turtle.pencolor(pen_color)
+    turtle.goto(start_x, start_y)
+    turtle.pendown()
+    turtle.goto(end_x, end_y)
+
+
+def draw_cross(center_x, center_y: int, size: int, pen_color):
+    draw_line(center_x, center_y, center_x + size, center_y + size, pen_color)
+    draw_line(center_x + size, center_y, center_x, center_y + size, pen_color)
+
+
+def write_text(start_x: int, start_y, text) -> turtle.Turtle:
+    erasable_text = turtle.Turtle()
+    erasable_text.penup()
+    erasable_text.setpos(start_x, start_y)
+    erasable_text.write(text, font=("Arial", 16, "normal"))
+    erasable_text.hideturtle()
+
+    return erasable_text
+
+
+def main():
+    # Draw base
+    draw_line(0, 0, 100, 0, DEFAULT_PEN_COLOR)
+
+    # Draw pole
+    draw_line(10, 0, 10, 100, DEFAULT_PEN_COLOR)
+
+    # Draw top
+    draw_line(10, 100, 50, 100, DEFAULT_PEN_COLOR)
+
+    # Draw rope
+    draw_line(50, 100, 50, 80, DEFAULT_PEN_COLOR)
+
+    # Draw head
+    draw_circle(50, 70, 10, DEFAULT_PEN_COLOR)
+
+    # Draw body
+    draw_line(50, 60, 50, 30, DEFAULT_PEN_COLOR)
+
+    # Draw hands
+    draw_line(50, 50, 40, 40, DEFAULT_PEN_COLOR)
+    draw_line(50, 50, 60, 40, DEFAULT_PEN_COLOR)
+
+    # Draw legs
+    draw_line(50, 30, 40, 20, DEFAULT_PEN_COLOR)
+    draw_line(50, 30, 60, 20, DEFAULT_PEN_COLOR)
+
+    # Draw eyes
+    draw_cross(45, 72, 2, DEFAULT_PEN_COLOR)
+    draw_cross(52, 72, 2, DEFAULT_PEN_COLOR)
+
+    turtle.hideturtle()
+
+    # turtle print
+    text = write_text(0, -25, "Hello World!")
+    time.sleep(2)
+    text.clear()
+
+    text = write_text(0, -25, "Goodbye World!")
+    time.sleep(2)
+    text.clear()
+
+    turtle.hideturtle()
+    turtle.done()
+
+
+if __name__ == "__main__":
+    main()
---
+++ hangman/hangman_graphics_template.py
@@ -0,0 +1,79 @@
+import time
+import turtle
+
+# Adjust the speed of the turtle 'fastest', 'fast', 'normal', 'slow', 'slowest'
+turtle.speed('slowest')
+
+# Available turtle shapes: 'arrow', 'turtle', 'circle', 'square', 'triangle', 'classic'
+turtle.shape('turtle')
+
+DEFAULT_PEN_COLOR = "black"
+
+def draw_circle(x, y, radius, pen_color):
+    # TODO: Draw a circle with the given parameters using turtle graphics
+    pass
+
+
+def draw_line(start_x, start_y, end_x, end_y, pen_color):
+    # TODO: Draw a line with the given parameters using turtle graphics
+    pass
+
+def draw_cross(center_x, center_y: int, size: int, pen_color):
+    # TODO: Draw a cross with the given parameters using turtle graphics
+    pass
+
+def write_text(start_x: int, start_y, text):
+    # TODO: Write the given text at the given position using turtle graphics
+    # Return the turtle object that writes the text
+    pass
+
+
+def main():
+    # TODO: Draw the base
+    # draw_line(, , , , )
+
+    # TODO: Draw the pole
+    # draw_line(, , , , )
+
+    # TODO: Draw the  upper beam
+    # draw_line(, , , , )
+
+    # TODO: Draw the  rope
+    # draw_line(, , , , )
+
+    # TODO: Draw the head
+    # draw_circle(, , , )
+
+    # TODO: Draw the body
+    # draw_line(, , , , )
+
+    # TODO: Draw arms
+    # draw_line(, , , , )
+    # draw_line(, , , , )
+
+    # TODO: Draw legs
+    # draw_line(, , , , )
+    # draw_line(, , , , )
+
+    # TODO: Draw the eyes
+    # draw_cross(, , , )
+    # draw_cross(, , , )
+
+    turtle.hideturtle()
+
+    # TODO: Write hello world
+    # text = write_text(, , )
+    # time.sleep(2)
+    # text.clear()
+
+    # TODO: Write goodbye world
+    # text = write_text(, , )
+    # time.sleep(2)
+    # text.clear()
+
+    turtle.hideturtle()
+    turtle.done()
+
+
+if __name__ == "__main__":
+    main()
